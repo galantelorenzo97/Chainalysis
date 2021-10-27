8b5e602962b273933bf324611b7cabe6897feaa2
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-27 15:27:44 -0400

    Added Gemini API to back-end

--- back-end/index.js
+++ back-end/index.js
@@ -16,8 +16,26 @@ app
             .then((results) => {
                 const buy = results[0].data.data.amount;
                 const sell = results[1].data.data.amount;
-                res.send({buy, sell});
+                res.send({
+                    exchange: "Coinbase", 
+                    info: "The Coinbase API includes the standard Coinbase fee (1%)", 
+                    buy, 
+                    sell
+                });
             });
+    })
+    .get('/getPricesfromGemini/:crypto', (req, res) => {
+        axios.get(`https://api.gemini.com/v1/pubticker/${req.params.crypto}USD`)
+        .then((response) => {
+            const buy = response.data.ask;
+            const sell = response.data.bid;
+            res.send({
+                exchange: "Gemini", 
+                warn: "The Gemini API does not include fees. As such, the reflected price may not be completely accurate. Be sure to visit Gemini's website for more information on fee schedules.", 
+                buy, 
+                sell
+            });
+        })
     });
 
 
