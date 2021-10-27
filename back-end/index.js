b0cd2f28bdc370608babdea77c8ad7c79d0a9bd9
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-27 00:32:30 -0400

    Added Coinbase API to back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
 app
-    .get('/', (req, res) => res.send('Placeholder'));
+    .get('/', (req, res) => res.send({
+        message: 'Hello World'
+    }))
+    .get('/getPricesfromCoinbase/:crypto', (req, res) => {
+        Promise.all([
+            axios.get(`https://api.coinbase.com/v2/prices/${req.params.crypto}-USD/buy`),
+            axios.get(`https://api.coinbase.com/v2/prices/${req.params.crypto}-USD/sell`)
+            ])
+            .then((results) => {
+                const buy = results[0].data.data.amount;
+                const sell = results[1].data.data.amount;
+                res.send({buy, sell});
+            });
+    });
+
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
