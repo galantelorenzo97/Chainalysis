4d576fda91c2cdafd8705ff18ce950289575e55d
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-28 00:55:38 -0400

    Created Navbar

---
+++ front-end/src/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/#">BTC/ETH Chainalysis recommender</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
