f1469db69dc325265885b9d92a48815eb6eac9e6
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-28 21:10:58 -0400

    Working pills

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,30 @@
 //import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const[buy, setBuy] = React.useState(true);
+  // eslint-disable-next-line
+  const[btc, setBtc] = React.useState(true);
   return (
     <div className="App">
       <header className="py-3 mb-4 border-bottom">
         <h1 className="display-1 text-center">
           I am looking to&nbsp;
           <div className="btn-group mh-100 mw-100" role="group" aria-label="Basic outlined example">
-            <button type="button" className="btn btn-danger active buy-btn">
+            <button onClick={()=>setBuy(true)} type="button" className={`btn ${buy ? "btn-danger active" : "btn-outline-danger"}`}>
               <h1 className="display-3">Buy</h1>
             </button>
-            <button type="button" className="btn btn-outline-success">
+            <button onClick={()=>setBuy(false)} type="button" className={`btn text-white ${!buy ? "btn-success active" : "btn-outline-success"}`}>
               <h1 className="display-3 active">Sell</h1>
             </button>
           </div>
           &nbsp;
           <div className="btn-group mh-100 mw-100" role="group" aria-label="Crypto-currency selector">
-            <button type="button" className="btn btn-outline-secondary">
+            <button onClick={()=>setBtc(true)} type="button" className={`btn ${btc ? "btn-primary active" : "btn-outline-primary"}`}>
               <h1 className="display-3">BTC</h1>
             </button>
-            <button type="button" className="btn btn-primary active">
+            <button onClick={()=>setBtc(false)} type="button" className={`btn ${!btc ? "btn-secondary active" : "btn-outline-secondary"}`}>
               <h1 className="display-3">ETH</h1>
             </button>
           </div>
