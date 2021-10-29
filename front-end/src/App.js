31a37210528c94c72aaa8c8271c607a65f559483
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-29 19:19:12 -0400

    Functional Listview, broken CoinCard

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,32 @@
 //import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
 import React from 'react';
 import Pill from './Pill.jsx'
+import CoinCard from './CoinCard.jsx';
 
 function App() {
   const [buy, setBuy] = React.useState(true);
   const [btc, setBtc] = React.useState(true);
+  const [exchangeLoaded, setExchangeLoaded] = React.useState(false);
+  const [exchangeDataList, setExchangeDataList] = React.useState([]);
+  const [currentlySelectedIndex, setCurrentlySelectedIndex] = React.useState(0);
+  const [currentlySelectedData, setCurrentlySelectedData] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(`http://localhost:8080/getAllPrices/${btc ? "BTC" : "ETH"}`)
+    .then(res => res.json())
+    .then((result) => {
+      setExchangeLoaded(true);
+      let sortedExchangeList = [...result];
+      let sortBuy = (a, b) => {return a.buy-b.buy};
+      let sortSell = (a, b) => {return b.sell-a.sell};
+      sortedExchangeList.sort(buy ? sortBuy : sortSell);
+      setExchangeDataList(sortedExchangeList);
+      console.log(sortedExchangeList);
+    })
+  }, [buy, btc]);
+
   return (
     <div className="App">
       <header className="py-3 mb-4">
@@ -34,6 +55,21 @@ function App() {
           />
         </h1>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <ul className="list-group">
+              {exchangeDataList.map((data, index) => (
+                <li onClick={(data, index) => {setCurrentlySelectedIndex(index); setCurrentlySelectedData(data);}} key={index} className={`list-group-item list-group-item-action ${index===0 ? "list-group-item-success" : "list-group-item-dark"} ${index===currentlySelectedIndex ? "active" : ""}`}>{data.exchange}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="col">
+            {console.log(exchangeDataList)}
+            <CoinCard exchange={exchangeDataList[currentlySelectedIndex].exchange} buy={1000} sell={2000} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- front-end/src/CoinCard.jsx
+++ front-end/src/CoinCard.jsx
@@ -1,10 +1,12 @@
 function CoinCard(props) {
     return (
         <div className="card text-white bg-dark mb-3">
-            <div class="card-header">{props.exchange}</div>
-            <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card-header">{props.exchange}</div>
+            <div className="card-body">
+                <h5 className="card-title">Light card title</h5>
+                <h6 className="card-subtitle mb-2 text-danger">BUY Price: ${props.buy}</h6>
+                <h6 className="card-subtitle mb-2 text-success">SELL Price: ${props.sell}</h6>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
     );
