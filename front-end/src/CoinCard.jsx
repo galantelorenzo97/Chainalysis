1839654327bf2381fd948c488b2d24dcd11f787c
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-29 22:05:59 -0400

    Modified CoinCard

--- front-end/src/CoinCard.jsx
+++ front-end/src/CoinCard.jsx
@@ -1,12 +1,19 @@
 function CoinCard(props) {
     return (
         <div className="card text-white bg-dark mb-3">
+            {props.logo &&
+                <img src={props.logo} class="card-img-top" alt={props.exchange + " logo"} />}
             <div className="card-header">{props.exchange}</div>
             <div className="card-body">
                 <h5 className="card-title">Light card title</h5>
                 <h6 className="card-subtitle mb-2 text-danger">BUY Price: ${props.buy}</h6>
                 <h6 className="card-subtitle mb-2 text-success">SELL Price: ${props.sell}</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                {props.warn &&
+                    <p className="card-text bg-warning">{props.warn}</p>
+                }
+                {props.info &&
+                    <p className="card-text bg-info">{props.info}</p>
+                }
             </div>
         </div>
     );
