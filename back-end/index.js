57fb0c8eb783d7417651dfea0f4bc4f3f53c69a7
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-29 22:50:33 -0400

    Added BitStamp and BitFinex API

--- back-end/index.js
+++ back-end/index.js
@@ -46,10 +46,42 @@ app
             });
         })
     })
+    .get('/getPricesFromBitfinex/:crypto', (req, res) => {
+        const bitfinexFeePercentage = 1.002;
+        axios.get(`https://api-pub.bitfinex.com/v2/ticker/t${req.params.crypto}USD`)
+        .then((response) => {
+            const buy = (response.data[0] * bitfinexFeePercentage).toFixed(2);
+            const sell = (response.data[2] * bitfinexFeePercentage).toFixed(2);
+            res.send({
+                exchange: "BitFinex", 
+                logo: "https://www.bitfinex.com/images/thumbnails/bitfinex-1.png",
+                warn: "BitFinex fee of 0.2% manually included.", 
+                buy, 
+                sell
+            });
+        })
+    })
+    .get('/getPricesFromBitstamp/:crypto', (req, res) => {
+        const bitstampFeePercentage = 1.005;
+        axios.get(`https://www.bitstamp.net/api/v2/ticker/${req.params.crypto.toLowerCase()}usd`)
+        .then((response) => {
+            const buy = Number((response.data.ask * bitstampFeePercentage).toFixed(2));
+            const sell = Number((response.data.bid * bitstampFeePercentage).toFixed(2));
+            res.send({
+                exchange: "BitStamp", 
+                logo: "https://www.paymentsjournal.com/wp-content/uploads/2017/12/bitstamp-logo.jpg",
+                warn: "BitStamp fee of 0.5% manually included.", 
+                buy, 
+                sell
+            });
+        })
+    })
     .get('/getAllPrices/:crypto', (req, res) => {
         Promise.all([
             axios.get(`http://localhost:${port}/getPricesFromCoinbase/${req.params.crypto}`),
-            axios.get(`http://localhost:${port}/getPricesFromGemini/${req.params.crypto}`)
+            axios.get(`http://localhost:${port}/getPricesFromGemini/${req.params.crypto}`),
+            axios.get(`http://localhost:${port}/getPricesFromBitfinex/${req.params.crypto}`),
+            axios.get(`http://localhost:${port}/getPricesFromBitstamp/${req.params.crypto}`)
         ])
         .then((results) => {
             let completeResults = [];
