90321c199ac3944484a00cb2e392d517ee15d559
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-10-29 23:15:42 -0400

    Removed unused/unworking dependencies

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './scss/custom.scss'
-import './index.css';
-//import Navbar from './Navbar.jsx'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
